0a2828575ffbecc58cc54fb6bf18ceb752dbc7a5
Mohammad Mahdi Asadi <mahdi.asadi2889@gmail.com>
2025-05-19 15:46:24 +0330

    Added LazyLoading

--- Routing-Exer/src/app/app.routes.ts
+++ Routing-Exer/src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '',component: HomeComponent},
-    {path: 'home',component: HomeComponent},
-    {path: 'about',component: AboutComponent},
-    {path: 'contact',component:ContactComponent}
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((a) => a.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./contact/contact.component').then((c) => c.ContactComponent),
+  },
 ];
